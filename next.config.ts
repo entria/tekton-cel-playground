0f63c638badc6697a7413f3db2af4927e96c71d0
Sibelius Seraphini <sibeliusseraphini@gmail.com>
2024-12-26 18:20:28 +0000

    feat(init): ༼ つ ◕_◕ ༽つ tekton cel playground

--- next.config.ts
+++ next.config.ts
@@ -2,6 +2,14 @@ import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
   /* config options here */
+  async rewrites() {
+    return [
+      {
+        source: '/api/evaluate', // The path in your Next.js app
+        destination: 'http://localhost:3002/api/evaluate', // Go API endpoint
+      },
+    ]
+  },
 };
 
 export default nextConfig;
